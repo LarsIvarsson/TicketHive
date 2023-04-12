4208fe0acc83d293075e6d0c346b1e4cf576133e
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-04-12 18:17:26 +0200

    Adding logic to change password and country

--- TicketHive/Server/Repo/IAppUserRepo.cs
+++ TicketHive/Server/Repo/IAppUserRepo.cs
@@ -2,8 +2,10 @@
 
 namespace TicketHive.Server.Repo
 {
-    public interface IAppUserRepo
-    {
-        Task<ApplicationUser?> GetUserByUsernameAsync(string AppUsername);
-    }
+	public interface IAppUserRepo
+	{
+		Task<ApplicationUser?> GetUserByUsernameAsync(string AppUsername);
+		Task<bool> PutAppUserAsync(string AppUsername, string Country);
+		Task<bool> ChangePasswordAsync(string AppUsername, string currentPassword, string newPassword);
+	}
 }
