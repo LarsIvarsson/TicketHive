b80a950e4063eb8765ef5c88031c17a704b7125e
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-04-13 23:18:23 +0200

    Increace, Decreace and Remove Functions. Not Dynamic

--- TicketHive/Client/Services/ICartService.cs
+++ TicketHive/Client/Services/ICartService.cs
@@ -2,17 +2,17 @@
 
 namespace TicketHive.Client.Services
 {
-    public interface ICartService
-    {
-        Task<List<CartItemsModel>> GetShoppingCartAsync(string userName);
+	public interface ICartService
+	{
+		Task<List<CartItemsModel>> GetShoppingCartAsync(string userName);
 
-        Task AddToCartAsync(string userName, EventModel addEvent);
+		Task AddToCartAsync(string userName, EventModel addEvent);
 
-        Task RemoveFromCartAsync(CartItemsModel removeEvent);
+		Task RemoveFromCartAsync(string userName, CartItemsModel removeEvent);
 
-        Task IncreaceQuantity(string userName, CartItemsModel item);
+		Task IncreaceQuantity(string userName, CartItemsModel item);
 
-        Task DecreaceQuantity(CartItemsModel item);
+		Task DecreaceQuantity(string userName, CartItemsModel item);
 
-    }
+	}
 }
