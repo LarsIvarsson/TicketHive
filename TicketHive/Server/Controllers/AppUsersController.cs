937c6ea40fd543800f58993ee696852dd12ffff0
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-04-14 12:06:43 +0200

    shoppingcart logic dynamic

--- TicketHive/Server/Controllers/AppUsersController.cs
+++ TicketHive/Server/Controllers/AppUsersController.cs
@@ -6,68 +6,68 @@ using TicketHive.Server.Repo;
 
 namespace TicketHive.Server.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AppUsersController : ControllerBase
-    {
-        private readonly IAppUserRepo repo;
-        public AppUsersController(IAppUserRepo repo)
-        {
-            this.repo = repo;
-        }
-        [HttpGet("{AppUsername}")]
-        public async Task<ActionResult<string?>> GetUserCountryByUsernameAsync(string AppUsername)
-        {
-            var result = await repo.GetUserByUsernameAsync(AppUsername);
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AppUsersController : ControllerBase
+	{
+		private readonly IAppUserRepo repo;
+		public AppUsersController(IAppUserRepo repo)
+		{
+			this.repo = repo;
+		}
+		[HttpGet("{AppUsername}")]
+		public async Task<ActionResult<string?>> GetUserCountryByUsernameAsync(string AppUsername)
+		{
+			var result = await repo.GetUserByUsernameAsync(AppUsername);
 
-            if (result != null)
-            {
-                string country = result.Country;
+			if (result != null)
+			{
+				string country = result.Country;
 
-                if (country != null)
-                {
-                    return Ok(country);
-                }
-            }
-            return NotFound();
-        }
-        [HttpPut("{AppUsername}")]
-        public async Task<IActionResult> ChangePasswordAsync(string AppUsername, [FromBody] string jsonList)
-        {
-            bool result;
-            List<string>? words = JsonConvert.DeserializeObject<List<string>>(jsonList);
-            if (words != null)
-            {
-                if (words.Count() > 1)
-                {
-                    result = await repo.ChangePasswordAsync(AppUsername, words[0], words[1]);
-                    if (result)
-                    {
-                        return Ok();
-                    }
-                }
-                result = await repo.PutAppUserAsync(AppUsername, words[0]);
-                if (result)
-                {
-                    return Ok();
-                }
-            }
-            return BadRequest();
-        }
+				if (country != null)
+				{
+					return Ok(country);
+				}
+			}
+			return NotFound();
+		}
+		[HttpPut("{AppUsername}")]
+		public async Task<IActionResult> ChangePasswordAsync(string AppUsername, [FromBody] string jsonList)
+		{
+			bool result;
+			List<string>? words = JsonConvert.DeserializeObject<List<string>>(jsonList);
+			if (words != null)
+			{
+				if (words.Count() > 1)
+				{
+					result = await repo.ChangePasswordAsync(AppUsername, words[0], words[1]);
+					if (result)
+					{
+						return Ok("Password was changed successfully!");
+					}
+				}
+				result = await repo.PutAppUserAsync(AppUsername, words[0]);
+				if (result)
+				{
+					return Ok("Country was changed succcessfully!");
+				}
+			}
+			return BadRequest("Something went wrong...");
+		}
 
-        // ==== not implemented (yet) ====
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
+		// ==== not implemented (yet) ====
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
 }
