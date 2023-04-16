0b9b05a665f9b8ebfe305fde6b40b74d05457020
LarsIvarsson <106492547+LarsIvarsson@users.noreply.github.com>
2023-04-16 16:37:19 +0200

    Adding SoldTickets to event model and some logic to increase this

--- TicketHive/Server/Repo/EventsRepo.cs
+++ TicketHive/Server/Repo/EventsRepo.cs
@@ -4,107 +4,112 @@ using TicketHive.Shared.Models;
 
 namespace TicketHive.Server.Repo
 {
-	public class EventsRepo : IEventsRepo
-	{
-		private readonly EventsDbContext context;
+    public class EventsRepo : IEventsRepo
+    {
+        private readonly EventsDbContext context;
 
-		private List<string> images = new()
-		{
-			"/images/event/img1.jpg",
-			"/images/event/img2.jpg",
-			"/images/event/img3.jpg",
-			"/images/event/img4.jpg",
-			"/images/event/img5.jpg",
-			"/images/event/img6.jpg",
-			"/images/event/img7.jpg",
-			"/images/event/img8.jpg",
-			"/images/event/img9.jpg",
-			"/images/event/img10.jpg"
-		};
+        private List<string> images = new()
+        {
+            "/images/event/img1.jpg",
+            "/images/event/img2.jpg",
+            "/images/event/img3.jpg",
+            "/images/event/img4.jpg",
+            "/images/event/img5.jpg",
+            "/images/event/img6.jpg",
+            "/images/event/img7.jpg",
+            "/images/event/img8.jpg",
+            "/images/event/img9.jpg",
+            "/images/event/img10.jpg"
+        };
 
-		public EventsRepo(EventsDbContext context)
-		{
-			this.context = context;
-		}
+        public EventsRepo(EventsDbContext context)
+        {
+            this.context = context;
+        }
 
-		public async Task<List<EventModel>?> GetEventsAsync()
-		{
-			return await context.Events.ToListAsync();
+        public async Task<List<EventModel>?> GetEventsAsync()
+        {
+            return await context.Events.ToListAsync();
 
-			/*.Include(e => e.EventUsers)*/
-		}
+            /*.Include(e => e.EventUsers)*/
+        }
 
-		public async Task<EventModel?> GetEventByIdAsync(int id)
-		{
-			return await context.Events.Include(e => e.EventUsers).FirstOrDefaultAsync(e => e.Id == id);
-			// Include(e => e.EventUsers).
-		}
+        public async Task<EventModel?> GetEventByIdAsync(int id)
+        {
+            return await context.Events.Include(e => e.EventUsers).FirstOrDefaultAsync(e => e.Id == id);
+            // Include(e => e.EventUsers).
+        }
 
-		public async Task<bool> PostEventAsync(EventModel model)
-		{
-			try
-			{
-				if (model.ImageUrl == null)
-				{
-					model.ImageUrl = SetImageUrl();
-				}
+        public async Task<bool> PostEventAsync(EventModel model)
+        {
+            try
+            {
+                if (model.ImageUrl == null)
+                {
+                    model.ImageUrl = SetImageUrl();
+                }
 
-				await context.Events.AddAsync(model);
-				await context.SaveChangesAsync();
-				return true;
-			}
+                await context.Events.AddAsync(model);
+                await context.SaveChangesAsync();
+                return true;
+            }
 
-			catch
-			{
-				return false;
-			}
-		}
+            catch
+            {
+                return false;
+            }
+        }
 
-		public async Task<bool> PutEventAsync(int id, EventModel model)
-		{
-			EventModel? modelToUpdate = await context.Events.FirstOrDefaultAsync(e => e.Id == id);
+        public async Task<bool> PutEventAsync(int id, EventModel model)
+        {
+            EventModel? modelToUpdate = await context.Events.FirstOrDefaultAsync(e => e.Id == id);
 
-			if (modelToUpdate != null)
-			{
-				modelToUpdate.Name = model.Name;
-				modelToUpdate.Type = model.Type;
-				modelToUpdate.Date = model.Date;
-				modelToUpdate.Venue = model.Venue;
-				modelToUpdate.Price = model.Price;
-				modelToUpdate.Capacity = model.Capacity;
-				modelToUpdate.ImageUrl = SetImageUrl();
+            if (modelToUpdate != null)
+            {
+                modelToUpdate.Name = model.Name;
+                modelToUpdate.Type = model.Type;
+                modelToUpdate.Date = model.Date;
+                modelToUpdate.Venue = model.Venue;
+                modelToUpdate.Price = model.Price;
+                modelToUpdate.Capacity = model.Capacity;
+                modelToUpdate.TicketsSold += model.TicketsSold;
+                if (modelToUpdate.ImageUrl == null)
+                {
+                    modelToUpdate.ImageUrl = SetImageUrl();
+                }
 
-				await context.SaveChangesAsync();
-				return true;
-			}
 
-			else
-			{
-				return false;
-			}
-		}
+                await context.SaveChangesAsync();
+                return true;
+            }
 
-		public async Task<bool> DeleteEventAsync(int id)
-		{
-			EventModel? modelToDelete = await context.Events.FirstOrDefaultAsync(e => e.Id == id);
+            else
+            {
+                return false;
+            }
+        }
 
-			if (modelToDelete != null)
-			{
-				context.Events.Remove(modelToDelete);
-				await context.SaveChangesAsync();
-				return true;
-			}
+        public async Task<bool> DeleteEventAsync(int id)
+        {
+            EventModel? modelToDelete = await context.Events.FirstOrDefaultAsync(e => e.Id == id);
 
-			else
-			{
-				return false;
-			}
-		}
+            if (modelToDelete != null)
+            {
+                context.Events.Remove(modelToDelete);
+                await context.SaveChangesAsync();
+                return true;
+            }
 
-		private string SetImageUrl()
-		{
-			int i = new Random().Next(0, 10);
-			return images[i];
-		}
-	}
+            else
+            {
+                return false;
+            }
+        }
+
+        private string SetImageUrl()
+        {
+            int i = new Random().Next(0, 10);
+            return images[i];
+        }
+    }
 }
--- TicketHive/Shared/Models/EventModel.cs
+++ TicketHive/Shared/Models/EventModel.cs
@@ -2,27 +2,25 @@
 
 namespace TicketHive.Shared.Models
 {
-	public class EventModel
-	{
-		[Key]
-		public int Id { get; set; }
-		[Required]
-		public string Name { get; set; } = null!;
-		[Required]
-		public CategoryEnum Type { get; set; }
-		[Required]
-		public DateTime Date { get; set; } = DateTime.Now;
-		[Required]
-		public string Venue { get; set; } = null!;
-		[Required]
-		public int Price { get; set; }
-		[Required]
-		public int Capacity { get; set; }
-		//public int TicketsSold { get; set; }
-		public bool IsSoldOut { get; set; }
-		// { return TicketsSold >= Capacity; } }
-
-		public List<UserModel>? EventUsers { get; set; } = new();
-		public string? ImageUrl { get; set; }
-	}
+    public class EventModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public CategoryEnum Type { get; set; }
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+        [Required]
+        public string Venue { get; set; } = null!;
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int Capacity { get; set; }
+        public int TicketsSold { get; set; }
+        public bool IsSoldOut => TicketsSold >= Capacity;
+        public List<UserModel>? EventUsers { get; set; } = new();
+        public string? ImageUrl { get; set; }
+    }
 }
